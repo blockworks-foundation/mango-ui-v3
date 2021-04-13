d97f63ced830f08786ea4ea8f7fa977e15c26f2e
Tyler Shipe <tjshipe@gmail.com>
2021-04-13 16:34:06 -0400

    add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,26 @@ function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keywords"
+          content="Mango Markets, Serum, SRM, Serum DEX, DEFI, Decentralized Finance, Decentralised Finance, Crypto, ERC20, Ethereum, Decentralize, Solana, SOL, SPL, Cross-Chain, Trading, Fastest, Fast, SerumBTC, SerumUSD, SRM Tokens, SPL Tokens"
+        />
+        <meta
+          name="description"
+          content="Mango Markets - Decentralised, cross-margin trading up to 5x leverage with lightning speed and near-zero fees powered by Serum."
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Mango Markets" />
+        <meta
+          name="twitter:description"
+          content="Mango Markets - Decentralised, cross-margin trading up to 5x leverage with lightning speed and near-zero fees powered by Serum."
+        />
+        <meta name="twitter:image" content="/twitter-image.png" />
 
         <script src="/datafeeds/udf/dist/polyfills.js"></script>
         <script src="/datafeeds/udf/dist/bundle.js"></script>
