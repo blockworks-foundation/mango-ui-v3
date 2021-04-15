07dcd658ea08ee919ef9cfe99ca18318eb512b1e
Tyler Shipe <tjshipe@gmail.com>
2021-04-15 16:06:42 -0400

    remove passhref

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -5,7 +5,7 @@ const MenuItem = ({ href, children }) => {
   const { asPath } = useRouter()
 
   return (
-    <Link href={href} passHref>
+    <Link href={href}>
       <a
         className={`block text-th-fgd-1 font-semibold items-center pl-3 pr-4 py-2 
           sm:inline-flex sm:ml-4 sm:px-1 sm:py-0 border-l-4 sm:border-l-0 sm:border-b-2 text-base tracking-wider hover:text-th-primary
