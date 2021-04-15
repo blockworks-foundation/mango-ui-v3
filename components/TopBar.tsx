5accf79de540ea2c9ba2d64733513e5de40ddc21
saml33 <slam.uke@gmail.com>
2021-04-15 20:08:57 +1000

    font-size class fix

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -13,7 +13,6 @@ import MenuItem from './MenuItem'
 import ThemeSwitch from './ThemeSwitch'
 import WalletIcon from './WalletIcon'
 import UiLock from './UiLock'
-import DropMenu from './DropMenu'
 import { useRouter } from 'next/router'
 import WalletSelect from './WalletSelect'
 import useMangoStore from '../stores/useMangoStore'
@@ -25,6 +24,10 @@ const Code = styled.code`
   border-radius: 3px;
   background: hsla(0, 0%, 58.8%, 0.1);
   font-size: 13px;
+`
+
+const StyledWalletTypeLabel = styled.div`
+  font-size: 0.6rem;
 `
 
 const TopBar = () => {
@@ -144,11 +147,11 @@ const TopBar = () => {
                         <WalletIcon className="w-5 h-5 mr-3 fill-current" />
                         <div>
                           Connect Wallet
-                          <div className="text-xxs font-normal text-th-fgd-1 text-left leading-3">
+                          <StyledWalletTypeLabel className="font-normal text-th-fgd-1 text-left leading-3">
                             {WALLET_PROVIDERS.filter(
                               (p) => p.url === savedProviderUrl
                             ).map(({ name }) => name)}
-                          </div>
+                          </StyledWalletTypeLabel>
                         </div>
                       </div>
                     </button>
