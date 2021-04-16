92eef50f5466375035c2b2dbd2b536b7f1d7b316
Tyler Shipe <tjshipe@gmail.com>
2021-04-16 11:04:04 -0400

    upgrade tailwind and next

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 // const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   future: {
     removeDeprecatedGapUtilities: true,
