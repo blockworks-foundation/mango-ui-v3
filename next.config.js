0e54dba8726ef61adeb0373fbcddf3a3389f6db4
Tyler Shipe <tjshipe@gmail.com>
2021-07-18 13:24:15 -0400

    ignore fs pkg in next.js and update to latest client

--- next.config.js
+++ next.config.js
@@ -7,7 +7,13 @@ module.exports = withBundleAnalyzer({
   typescript: {
     ignoreBuildErrors: true, // TODO: remove this before mainnet
   },
-  webpack(config) {
+  webpack(config, { isServer }) {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
