77716dc7ee9278289e29ac20c1805edcde8669a6
Tyler Shipe <tjshipe@gmail.com>
2021-08-04 14:51:17 -0400

    fix account name

--- hooks/useWallet.tsx
+++ hooks/useWallet.tsx
@@ -17,6 +17,12 @@ const ASSET_URL =
 
 export const WALLET_PROVIDERS = [
   {
+    name: 'Phantom',
+    url: 'https://www.phantom.app',
+    icon: `https://www.phantom.app/img/logo.png`,
+    adapter: PhantomWalletAdapter,
+  },
+  {
     name: 'Sollet.io',
     url: 'https://www.sollet.io',
     icon: `${ASSET_URL}/sollet.svg`,
@@ -26,12 +32,6 @@ export const WALLET_PROVIDERS = [
     url: 'https://www.sollet.io/extension',
     icon: `${ASSET_URL}/sollet.svg`,
     adapter: SolletExtensionAdapter as any,
-  },
-  {
-    name: 'Phantom',
-    url: 'https://www.phantom.app',
-    icon: `https://www.phantom.app/img/logo.png`,
-    adapter: PhantomWalletAdapter,
   },
 ]
 
