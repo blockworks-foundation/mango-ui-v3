2e83ad6805182ca8eabb0c39a9da178ece9e6385
Tyler Shipe <tjshipe@gmail.com>
2021-08-13 18:14:44 -0400

    tweak page container width

--- components/PageBodyContainer.tsx
+++ components/PageBodyContainer.tsx
@@ -1,6 +1,6 @@
 const PageBodyContainer = ({ children }) => (
   <div className="min-h-screen grid grid-cols-12 gap-4 pb-10">
-    <div className="col-span-12 px-6 xl:col-start-2 xl:col-span-10 2xl:col-start-3 2xl:col-span-8">
+    <div className="col-span-12 px-4 xl:col-start-1 xl:col-span-11 2xl:col-start-3 2xl:col-span-8">
       {children}
     </div>
   </div>
