8ac108ce354f9b9e8d39ff2dea5ea4e5abb7ab00
Tyler Shipe <tjshipe@gmail.com>
2021-08-25 14:47:57 -0400

    update alpha modal

--- components/AlphaModal.tsx
+++ components/AlphaModal.tsx
@@ -25,29 +25,21 @@ const AlphaModal = ({
         <div className="flex flex-col items-center">
           <div className="flex space-x-8 items-center justify-center ">
             <img
-              className={`h-12 w-auto mb-2`}
+              className={`h-12 w-auto`}
               src="/assets/icons/logo.svg"
               alt="next"
             />
           </div>
         </div>
       </Modal.Header>
-      <div className={`text-th-fgd-2 text-center`}>
-        SOL and SRM now available with USDC!
+      <div className={`text-th-fgd-2 text-center text-xl text-strong`}>
+        Perps are now available!
       </div>
       <div className="text-th-fgd-2 text-center my-4">
-        Visit{' '}
-        <a
-          href="https://usdt.mango.markets"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          https://usdt.mango.markets
-        </a>{' '}
-        to access the USDT mango group.
+        Welcome to V3 Alpha. Mango Markets is unaudited software, use at your
+        own risk. Have fun, dont get liquidated, or do, we need to test that :)
       </div>
       <div className={`text-th-fgd-2 text-center`}>
-        Mango Markets is unaudited software. Use at your own risk.
         <div className={`mt-4 flex justify-center`}>
           <Button onClick={handleAccept}>
             <div className={`flex items-center`}>Accept</div>
