44e0d8ad217690e8d8fbfaab64af73cd5be4c2cb
Tyler Shipe <tjshipe@gmail.com>
2021-08-25 17:07:25 -0400

    make it more obvious when opening new position with slider

--- components/LeverageSlider.tsx
+++ components/LeverageSlider.tsx
@@ -142,13 +142,13 @@ export default function LeverageSlider({
 
   const closeDepositString =
     percentToClose(value, roundedDeposits) > 100
-      ? '100% Close Position'
-      : `${percentToClose(value, roundedDeposits).toFixed(2)}% Close Position`
+      ? '100% Close Position + Open Short'
+      : `${percentToClose(value, roundedDeposits).toFixed(1)}% Close Position`
 
   const closeBorrowString =
     percentToClose(value, roundedBorrows) > 100
-      ? '100% Close Position'
-      : `${percentToClose(value, roundedBorrows).toFixed(2)}% Close Position`
+      ? '100% Close Position + Open Long'
+      : `${percentToClose(value, roundedBorrows).toFixed(1)}% Close Position`
 
   const setMaxLeverage = function () {
     onChange(Math.round(max / step) * step)
