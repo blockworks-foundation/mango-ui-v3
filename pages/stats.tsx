03763092600c9d42334006ab242b3064c890d48e
Tyler Shipe <tjshipe@gmail.com>
2021-09-01 11:54:22 -0400

    strip stats page to test on mobile

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import TopBar from '../components/TopBar'
 import PageBodyContainer from '../components/PageBodyContainer'
-import StatsTotals from '../components/stats-page/StatsTotals'
-import StatsAssets from '../components/stats-page/StatsAssets'
-import StatsPerps from '../components/stats-page/StatsPerps'
-import useMangoStats from '../hooks/useMangoStats'
+// import StatsTotals from '../components/stats-page/StatsTotals'
+// import StatsAssets from '../components/stats-page/StatsAssets'
+// import StatsPerps from '../components/stats-page/StatsPerps'
+// import useMangoStats from '../hooks/useMangoStats'
 
-const TABS = [
-  'Totals',
-  'Assets',
-  // 'Perps',
-  // 'Markets',
-  // 'Liquidations',
-]
+// const TABS = [
+//   'Totals',
+//   'Assets',
+//   // 'Perps',
+//   // 'Markets',
+//   // 'Liquidations',
+// ]
 
 export default function StatsPage() {
   console.log('stats loaded')
-  const { latestStats, stats } = useMangoStats()
-  const [activeTab, setActiveTab] = useState(TABS[1])
+  // const { latestStats, stats } = useMangoStats()
+  // const [activeTab, setActiveTab] = useState(TABS[1])
 
-  const handleTabChange = (tabName) => {
-    setActiveTab(tabName)
-  }
+  // const handleTabChange = (tabName) => {
+  //   setActiveTab(tabName)
+  // }
 
   return (
     <div className={`bg-th-bkg-1 text-th-fgd-1 transition-all`}>
@@ -30,7 +30,7 @@ export default function StatsPage() {
         <div className="flex flex-col sm:flex-row pt-8 pb-3 sm:pb-6 md:pt-10">
           <h1 className={`text-th-fgd-1 text-2xl font-semibold`}>Stats</h1>
         </div>
-        <div className="bg-th-bkg-2 overflow-none p-6 rounded-lg">
+        {/* <div className="bg-th-bkg-2 overflow-none p-6 rounded-lg">
           <div className="border-b border-th-fgd-4 mb-4">
             <nav className={`-mb-px flex space-x-6`} aria-label="Tabs">
               {TABS.map((tabName) => (
@@ -55,25 +55,25 @@ export default function StatsPage() {
             latestStats={latestStats}
             stats={stats}
           />
-        </div>
+        </div> */}
       </PageBodyContainer>
     </div>
   )
 }
 
-const TabContent = ({ activeTab, latestStats, stats }) => {
-  switch (activeTab) {
-    case 'Totals':
-      return <StatsTotals latestStats={latestStats} stats={stats} />
-    case 'Assets':
-      return <StatsAssets latestStats={latestStats} stats={stats} />
-    case 'Perps':
-      return <StatsPerps />
-    case 'Markets':
-      return <div>Markets</div>
-    case 'Liquidations':
-      return <div>Liquidations</div>
-    default:
-      return <StatsAssets latestStats={latestStats} stats={stats} />
-  }
-}
+// const TabContent = ({ activeTab, latestStats, stats }) => {
+//   switch (activeTab) {
+//     case 'Totals':
+//       return <StatsTotals latestStats={latestStats} stats={stats} />
+//     case 'Assets':
+//       return <StatsAssets latestStats={latestStats} stats={stats} />
+//     case 'Perps':
+//       return <StatsPerps />
+//     case 'Markets':
+//       return <div>Markets</div>
+//     case 'Liquidations':
+//       return <div>Liquidations</div>
+//     default:
+//       return <StatsAssets latestStats={latestStats} stats={stats} />
+//   }
+// }
