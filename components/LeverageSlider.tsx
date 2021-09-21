1fdea822cd75320991530f52e3cfaf0bbd4c9674
Tyler Shipe <tjshipe@gmail.com>
2021-09-21 16:12:50 -0400

    leverage slider should use limit price if entered

--- components/LeverageSlider.tsx
+++ components/LeverageSlider.tsx
@@ -114,8 +114,7 @@ export default function LeverageSlider({
     // srm fees or rounding issues in getMaxLeverageForMarket
     const maxScaler = market instanceof PerpMarket ? 0.99 : 0.95
     const scaledMax =
-      (maxQuote.toNumber() * maxScaler) /
-      mangoGroup.getPrice(marketIndex, mangoCache).toNumber()
+      (maxQuote.toNumber() * maxScaler) / priceOrDefault.toNumber()
 
     return { max: scaledMax, deposits, borrows }
   }, [mangoAccount, mangoGroup, mangoCache, marketIndex, market, side, price])
