33fa3a2f7196301704f3f07855cec250f4a0e72f
saml33 <slam.uke@gmail.com>
2021-11-29 20:35:00 +1100

    add modify order to mobile table

--- components/OpenOrdersTable.tsx
+++ components/OpenOrdersTable.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-// import { CheckIcon, XIcon } from '@heroicons/react/outline'
+import { PencilIcon, TrashIcon, XIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useOpenOrders } from '../hooks/useOpenOrders'
-import Button from './Button'
+import Button, { IconButton } from './Button'
 import Loading from './Loading'
 import useMangoStore from '../stores/useMangoStore'
 import { notify } from '../utils/notifications'
@@ -13,7 +13,6 @@ import {
   PerpOrder,
   PerpMarket,
   MarketConfig,
-  sleep,
 } from '@blockworks-foundation/mango-client'
 import { formatUsdValue, getDecimalCount, usdFormatter } from '../utils'
 import { Table, Td, Th, TrBody, TrHead } from './TableElements'
@@ -26,17 +25,24 @@ import Input from './Input'
 
 const DesktopTable = ({
   cancelledOrderId,
-  editOrder,
+  editOrderIndex,
   handleCancelOrder,
   handleModifyOrder,
   modifiedOrderId,
   openOrders,
-  setEditOrder,
+  setEditOrderIndex,
 }) => {
   const { t } = useTranslation('common')
   const { asPath } = useRouter()
   const [modifiedOrderSize, setModifiedOrderSize] = useState('')
   const [modifiedOrderPrice, setModifiedOrderPrice] = useState('')
+
+  const showEditOrderForm = (index, order) => {
+    setEditOrderIndex(index)
+    setModifiedOrderSize(order.size)
+    setModifiedOrderPrice(order.price)
+  }
+
   const renderMarketName = (market: MarketConfig) => {
     const location = `/${market.kind}/${market.baseSymbol}`
     if (!asPath.includes(location)) {
@@ -69,6 +75,7 @@ const DesktopTable = ({
       <tbody>
         {openOrders.map(({ order, market }, index) => {
           const decimals = getDecimalCount(market.account.tickSize)
+          const editThisOrder = editOrderIndex === index
           return (
             <TrBody index={index} key={`${order.orderId}${order.side}`}>
               <Td className="w-[14.286%]">
@@ -86,7 +93,7 @@ const DesktopTable = ({
               <Td className="w-[14.286%]">
                 <SideBadge side={order.side} />
               </Td>
-              {!editOrder ? (
+              {editOrderIndex !== index ? (
                 <>
                   <Td className="w-[14.286%]">{order.size}</Td>
                   <Td className="w-[14.286%]">
@@ -99,7 +106,7 @@ const DesktopTable = ({
                     <Input
                       className="bg-transparent border-b-2 border-l-0 border-r-0 border-t-0 h-7 px-0 rounded-none"
                       type="number"
-                      value={modifiedOrderSize || order.size}
+                      value={modifiedOrderSize}
                       onChange={(e) => setModifiedOrderSize(e.target.value)}
                     />
                   </Td>
@@ -107,31 +114,33 @@ const DesktopTable = ({
                     <Input
                       className="bg-transparent border-b-2 border-l-0 border-r-0 border-t-0 h-7 px-0 rounded-none"
                       type="number"
-                      value={modifiedOrderPrice || order.price}
+                      value={modifiedOrderPrice}
                       onChange={(e) => setModifiedOrderPrice(e.target.value)}
                     />
                   </Td>
                 </>
               )}
               <Td className="w-[14.286%]">
-                {editOrder ? '' : formatUsdValue(order.price * order.size)}
+                {editThisOrder ? '' : formatUsdValue(order.price * order.size)}
               </Td>
               <Td className="w-[14.286%]">
                 {order.perpTrigger &&
                   `${order.orderType} ${
                     order.triggerCondition
-                  } ${order.triggerPrice.toFixed(2)}`}
+                  } $${order.triggerPrice.toFixed(2)}`}
               </Td>
               <Td className="w-[14.286%]">
                 <div className={`flex justify-end space-x-3`}>
-                  {!editOrder ? (
+                  {editOrderIndex !== index ? (
                     <>
-                      <Button
-                        onClick={() => setEditOrder(!editOrder)}
-                        className="text-xs pt-0 pb-0 h-8 pl-3 pr-3"
-                      >
-                        {t('edit')}
-                      </Button>
+                      {!order.perpTrigger ? (
+                        <Button
+                          onClick={() => showEditOrderForm(index, order)}
+                          className="text-xs pt-0 pb-0 h-8 pl-3 pr-3"
+                        >
+                          {t('edit')}
+                        </Button>
+                      ) : null}
                       <Button
                         onClick={() => handleCancelOrder(order, market.account)}
                         className="text-xs pt-0 pb-0 h-8 pl-3 pr-3"
@@ -151,7 +160,8 @@ const DesktopTable = ({
                           handleModifyOrder(
                             order,
                             market.account,
-                            modifiedOrderPrice
+                            modifiedOrderPrice || order.price,
+                            modifiedOrderSize || order.size
                           )
                         }
                       >
@@ -163,7 +173,7 @@ const DesktopTable = ({
                       </Button>
                       <Button
                         className="text-xs pt-0 pb-0 h-8 pl-3 pr-3"
-                        onClick={() => setEditOrder(false)}
+                        onClick={() => setEditOrderIndex(null)}
                       >
                         Cancel Edit
                       </Button>
@@ -179,54 +189,139 @@ const DesktopTable = ({
   )
 }
 
-const MobileTable = ({ openOrders, cancelledOrderId, handleCancelOrder }) => {
+const MobileTable = ({
+  cancelledOrderId,
+  editOrderIndex,
+  handleCancelOrder,
+  handleModifyOrder,
+  modifiedOrderId,
+  openOrders,
+  setEditOrderIndex,
+}) => {
   const { t } = useTranslation('common')
+  const [modifiedOrderSize, setModifiedOrderSize] = useState('')
+  const [modifiedOrderPrice, setModifiedOrderPrice] = useState('')
+
+  const showEditOrderForm = (index) => {
+    setEditOrderIndex(index)
+    setModifiedOrderSize('')
+    setModifiedOrderPrice('')
+  }
+
   return (
     <>
-      {openOrders.map(({ market, order }, index) => (
-        <Row key={`${order.orderId}${order.side}`} index={index}>
-          <div className="col-span-12 flex items-center justify-between text-fgd-1 text-left">
-            <div className="flex items-center">
-              <img
-                alt=""
-                width="20"
-                height="20"
-                src={`/assets/icons/${market.config.baseSymbol.toLowerCase()}.svg`}
-                className={`mr-2.5`}
-              />
-              <div>
-                <div className="mb-0.5">{market.config.name}</div>
-                <div className="text-th-fgd-3 text-xs">
-                  <span
-                    className={`mr-1
+      {openOrders.map(({ market, order }, index) => {
+        const editThisOrder = editOrderIndex === index
+        return (
+          <Row key={`${order.orderId}${order.side}`} index={index}>
+            <div className="col-span-12 flex items-center justify-between text-fgd-1 text-left">
+              <div className="flex items-center">
+                <img
+                  alt=""
+                  width="20"
+                  height="20"
+                  src={`/assets/icons/${market.config.baseSymbol.toLowerCase()}.svg`}
+                  className={`mr-2.5`}
+                />
+                <div>
+                  <div className="mb-0.5">{market.config.name}</div>
+                  <div className="text-th-fgd-3 text-xs">
+                    <span
+                      className={`mr-1
                                 ${
                                   order.side === 'buy'
                                     ? 'text-th-green'
                                     : 'text-th-red'
                                 }
                               `}
-                  >
-                    {order.side.toUpperCase()}
-                  </span>
-                  {order.perpTrigger
-                    ? `${order.size} ${order.triggerCondition} ${order.triggerPrice}`
-                    : `${order.size} at ${formatUsdValue(order.price)}`}
+                    >
+                      {order.side.toUpperCase()}
+                    </span>
+                    {order.perpTrigger
+                      ? `${order.size} ${
+                          order.triggerCondition
+                        } ${formatUsdValue(order.triggerPrice)}`
+                      : `${order.size} at ${formatUsdValue(order.price)}`}
+                  </div>
                 </div>
               </div>
+              <div className="flex space-x-2">
+                {!order.perpTrigger ? (
+                  <IconButton
+                    className={index % 2 === 0 ? 'bg-th-bkg-4' : 'bg-th-bkg-3'}
+                    onClick={() =>
+                      editThisOrder
+                        ? setEditOrderIndex(null)
+                        : showEditOrderForm(index)
+                    }
+                  >
+                    {editThisOrder ? (
+                      <XIcon className="h-5 w-5" />
+                    ) : (
+                      <PencilIcon className="h-5 w-5" />
+                    )}
+                  </IconButton>
+                ) : null}
+                <IconButton
+                  className={index % 2 === 0 ? 'bg-th-bkg-4' : 'bg-th-bkg-3'}
+                  onClick={() => handleCancelOrder(order, market.account)}
+                >
+                  {cancelledOrderId + '' === order.orderId + '' ? (
+                    <Loading />
+                  ) : (
+                    <TrashIcon className="h-5 w-5" />
+                  )}
+                </IconButton>
+              </div>
             </div>
-            <Button
-              onClick={() => handleCancelOrder(order, market.account)}
-              className="ml-3 text-xs pt-0 pb-0 h-8 pl-3 pr-3"
-            >
-              {cancelledOrderId + '' === order.orderId + '' ? (
-                <Loading />
-              ) : (
-                <span>{t('cancel')}</span>
-              )}
-            </Button>
-          </div>
-        </Row>
-      ))}
+            {editThisOrder ? (
+              <div className="flex flex-col sm:flex-row pt-4 sm:space-x-3">
+                <div>
+                  <label className="mb-2 text-th-fgd-3 text-xs">
+                    {t('size')}
+                  </label>
+                  <Input
+                    className="bg-transparent border-b-2 border-l-0 border-r-0 border-t-0 h-7 mb-4 sm:mb-0 px-0 rounded-none"
+                    type="number"
+                    value={modifiedOrderSize || order.size}
+                    onChange={(e) => setModifiedOrderSize(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label className="mb-2 text-th-fgd-3 text-xs">
+                    {t('price')}
+                  </label>
+                  <Input
+                    className="bg-transparent border-b-2 border-l-0 border-r-0 border-t-0 h-7 px-0 rounded-none"
+                    type="number"
+                    value={modifiedOrderPrice || order.price}
+                    onChange={(e) => setModifiedOrderPrice(e.target.value)}
+                  />
+                </div>
+                <Button
+                  className={`${
+                    index % 2 === 0 ? 'bg-th-bkg-4' : 'bg-th-bkg-3'
+                  } text-xs mt-4 pt-0 pb-0 h-8 pl-3 pr-3`}
+                  onClick={() =>
+                    handleModifyOrder(
+                      order,
+                      market.account,
+                      modifiedOrderPrice || order.price,
+                      modifiedOrderSize || order.size
+                    )
+                  }
+                >
+                  {modifiedOrderId + '' === order.orderId + '' ? (
+                    <Loading />
+                  ) : (
+                    <span>{t('save')}</span>
+                  )}
+                </Button>
+              </div>
+            ) : null}
+          </Row>
+        )
+      })}
     </>
   )
 }
@@ -237,7 +332,7 @@ const OpenOrdersTable = () => {
   const openOrders = useOpenOrders()
   const [cancelId, setCancelId] = useState(null)
   const [modifyId, setModifyId] = useState(null)
-  const [editOrder, setEditOrder] = useState(false)
+  const [editOrderIndex, setEditOrderIndex] = useState(null)
   const actions = useMangoStore((s) => s.actions)
   const { width } = useViewport()
   const isMobile = width ? width < breakpoints.md : false
@@ -294,8 +389,9 @@ const OpenOrdersTable = () => {
       })
       console.log('error', `${e}`)
     } finally {
-      // await sleep(600)
-      actions.reloadMangoAccount()
+      await actions.fetchMangoGroup()
+      await actions.reloadMangoAccount()
+      await actions.reloadOrders()
       setCancelId(null)
     }
   }
@@ -303,7 +399,8 @@ const OpenOrdersTable = () => {
   const handleModifyOrder = async (
     order: Order | PerpOrder,
     market: Market | PerpMarket,
-    price: number
+    price: number,
+    size: number
   ) => {
     const mangoAccount = useMangoStore.getState().selectedMangoAccount.current
     const mangoGroup = useMangoStore.getState().selectedMangoGroup.current
@@ -335,7 +432,7 @@ const OpenOrdersTable = () => {
           order as Order,
           order.side,
           orderPrice,
-          order.size,
+          size,
           orderType
         )
       } else {
@@ -348,13 +445,12 @@ const OpenOrdersTable = () => {
           order as PerpOrder,
           order.side,
           orderPrice,
-          order.size,
+          size,
           orderType,
           0,
           order.side === 'buy' ? askInfo : bidInfo
         )
       }
-      setEditOrder(false)
       notify({ title: t('successfully-placed'), txid })
     } catch (e) {
       notify({
@@ -364,21 +460,22 @@ const OpenOrdersTable = () => {
         type: 'error',
       })
     } finally {
-      sleep(600).then(() => {
-        actions.reloadMangoAccount()
-        setModifyId(null)
-      })
+      await actions.fetchMangoGroup()
+      await actions.reloadMangoAccount()
+      await actions.reloadOrders()
+      setModifyId(null)
+      setEditOrderIndex(null)
     }
   }
 
   const tableProps = {
     openOrders,
     cancelledOrderId: cancelId,
-    editOrder,
+    editOrderIndex,
     handleCancelOrder,
     handleModifyOrder,
     modifiedOrderId: modifyId,
-    setEditOrder,
+    setEditOrderIndex,
   }
 
   return (
