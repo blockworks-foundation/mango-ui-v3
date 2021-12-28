485c2eff4ec83951094949eae09a777e3f86d149
Tyler Shipe <tjshipe@gmail.com>
2021-12-28 12:13:04 -0500

    use getStaticProps in calculator

--- pages/risk-calculator.tsx
+++ pages/risk-calculator.tsx
@@ -28,7 +28,7 @@ import { AnchorIcon } from '../components/icons'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 
-export async function getServerSideProps({ locale }) {
+export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
