f367b47829477941690631204b4099a899c6f87a
saml33 <slam.uke@gmail.com>
2022-02-21 21:21:16 +1100

    fix hover style

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -102,7 +102,7 @@ const ConnectWalletButton = () => {
           <button
             onClick={handleWalletConect}
             disabled={!wallet || !mangoGroup}
-            className="bg-th-primary-dark rounded-none text-th-bkg-1 hover:brightness-[1.15] focus:outline-none disabled:text-th-bkg-2 disabled:cursor-wait"
+            className="bg-th-primary-dark rounded-none text-th-bkg-1 hover:brightness-[1.1] focus:outline-none disabled:text-th-bkg-2 disabled:cursor-wait"
           >
             <div className="flex flex-row items-center px-3 justify-center h-full default-transition">
               <WalletIcon className="w-4 h-4 mr-2 fill-current" />
