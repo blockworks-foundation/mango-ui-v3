a46c9a35961b0d4435e21fbba954cb42b8023e11
Kieran Gillen <kgillen@gmail.com>
2022-03-25 12:20:06 +0100

    Removes commented code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const { i18n } = require('./next-i18next.config')
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// })
-
 const moduleExports = {
   i18n,
   async redirects() {
