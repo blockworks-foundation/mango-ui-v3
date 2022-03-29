8dc166d94d29a3de9ebafcc6116f97ed962879f8
Luc Succes <l2succes@gmail.com>
2022-03-29 08:23:18 -0400

    Remove comment

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -2,57 +2,6 @@ import React from 'react'
 import { FiveOhFive } from './FiveOhFive'
 import * as Sentry from '@sentry/react'
 
-// class ErrorBoundary extends React.Component<
-//   any,
-//   { hasError: boolean; error: any }
-// > {
-//   constructor(props) {
-//     super(props)
-//     this.state = { hasError: false, error: null }
-//   }
-
-//   static getDerivedStateFromError(error) {
-//     // Update state so the next render will show the fallback UI.
-//     return { hasError: true, error: error }
-//   }
-
-//   componentDidCatch(error, errorInfo) {
-//     // You can also log the error to an error reporting service
-//     // logErrorToMyService(error, errorInfo)
-
-//     if (process.env.NEXT_PUBLIC_ERROR_WEBHOOK_URL) {
-//       try {
-//         fetch(process.env.NEXT_PUBLIC_ERROR_WEBHOOK_URL, {
-//           method: 'POST',
-//           headers: { 'Content-Type': 'application/json' },
-//           body: JSON.stringify({
-//             content: `UI ERROR: ${error} : ${errorInfo?.componentStack}`.slice(
-//               0,
-//               1999
-//             ),
-//           }),
-//         })
-//       } catch (err) {
-//         console.error('Error posting to notify webhook:', err)
-//       }
-//     }
-//   }
-
-//   render() {
-//     let children = this.props.children
-//     if (this.state.hasError) {
-//       // You can render any custom fallback UI
-//       children = <FiveOhFive error={this.state.error} />
-//     }
-
-//     return (
-//       <Sentry.ErrorBoundary fallback={(error, componentStack)}>
-//         {children}
-//       </Sentry.ErrorBoundary>
-//     )
-//   }
-// }
-
 const ErrorBoundary: React.FC<any> = (props) => {
   const postError = (error, componentStack) => {
     if (process.env.NEXT_PUBLIC_ERROR_WEBHOOK_URL) {
