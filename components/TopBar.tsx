19da795035fb8967ab4a9695a1dedd9f2c9b64f0
tjs <tjshipe@gmail.com>
2022-04-03 12:16:11 -0400

    remove new badge from topbar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -21,11 +21,11 @@ import {
 import { MangoIcon } from './icons'
 import { useWallet } from '@solana/wallet-adapter-react'
 
-const StyledNewLabel = ({ children, ...props }) => (
-  <div style={{ fontSize: '0.5rem', marginLeft: '1px' }} {...props}>
-    {children}
-  </div>
-)
+// const StyledNewLabel = ({ children, ...props }) => (
+//   <div style={{ fontSize: '0.5rem', marginLeft: '1px' }} {...props}>
+//     {children}
+//   </div>
+// )
 
 const TopBar = () => {
   const { t } = useTranslation('common')
@@ -65,16 +65,7 @@ const TopBar = () => {
               >
                 <TradeNavMenu />
                 <MenuItem href="/account">{t('account')}</MenuItem>
-                <div className="relative">
-                  <MenuItem href="/markets">
-                    {t('markets')}
-                    <div className="absolute -right-3 -top-3 flex h-4 items-center justify-center rounded-full bg-gradient-to-br from-red-500 to-yellow-500 px-1.5">
-                      <StyledNewLabel className="uppercase text-white">
-                        new
-                      </StyledNewLabel>
-                    </div>
-                  </MenuItem>
-                </div>
+                <MenuItem href="/markets">{t('markets')}</MenuItem>
                 <MenuItem href="/borrow">{t('borrow')}</MenuItem>
                 <MenuItem href="/swap">{t('swap')}</MenuItem>
                 <MenuItem href="/stats">{t('stats')}</MenuItem>
