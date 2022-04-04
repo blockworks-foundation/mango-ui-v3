bc3f63802653de2f5b84e0bd37791d34997737e3
tjs <tjshipe@gmail.com>
2022-04-03 22:46:38 -0400

    improve notification animations

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { Fragment, useEffect } from 'react'
 import {
   CheckCircleIcon,
   ExternalLinkIcon,
@@ -9,6 +9,7 @@ import useMangoStore, { CLUSTER } from '../stores/useMangoStore'
 import { Notification, notify } from '../utils/notifications'
 import { useTranslation } from 'next-i18next'
 import Loading from './Loading'
+import { Transition } from '@headlessui/react'
 
 const NotificationList = () => {
   const { t } = useTranslation('common')
@@ -95,7 +96,7 @@ const Notification = ({ notification }: { notification: Notification }) => {
     })
   }
 
-  // auto hide a notification after 10 seconds unless it is a confirming or time out notification
+  // auto hide a notification after 8 seconds unless it is a confirming or time out notification
   useEffect(() => {
     const id = setTimeout(
       () => {
@@ -103,7 +104,7 @@ const Notification = ({ notification }: { notification: Notification }) => {
           hideNotification()
         }
       },
-      parsedTitle || type === 'confirm' || type === 'error' ? 45000 : 8000
+      parsedTitle || type === 'confirm' || type === 'error' ? 40000 : 8000
     )
 
     return () => {
@@ -111,77 +112,92 @@ const Notification = ({ notification }: { notification: Notification }) => {
     }
   })
 
-  if (!show) return null
-
   return (
-    <div
-      className={`pointer-events-auto mt-2 w-full max-w-sm overflow-hidden rounded-md border border-th-bkg-4 bg-th-bkg-3 shadow-lg ring-1 ring-black ring-opacity-5`}
+    <Transition
+      show={show}
+      as={Fragment}
+      appear={true}
+      enter="transform ease-out duration-500 transition"
+      enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:-translate-x-8"
+      enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+      leave="transform ease-in duration-300 transition"
+      leaveFrom="translate-y-0 opacity-100 sm:translate-x-0"
+      leaveTo="-translate-y-2 opacity-0 sm:translate-y-0 sm:-translate-x-8"
     >
-      <div className={`relative flex items-center px-2 py-2.5`}>
-        <div className={`flex-shrink-0`}>
-          {type === 'success' ? (
-            <CheckCircleIcon className={`mr-1 h-7 w-7 text-th-green`} />
-          ) : null}
-          {type === 'info' && (
-            <InformationCircleIcon className={`mr-1 h-7 w-7 text-th-primary`} />
-          )}
-          {type === 'error' && (
-            <XCircleIcon className={`mr-1 h-7 w-7 text-th-red`} />
-          )}
-          {type === 'confirm' && (
-            <Loading className="mr-1 h-7 w-7 text-th-fgd-3" />
-          )}
-        </div>
-        <div className={`ml-2 flex-1`}>
-          <div className={`text-normal font-bold text-th-fgd-1`}>
-            {parsedTitle || title}
-          </div>
-          {description ? (
-            <p className={`mb-0 mt-0.5 leading-tight text-th-fgd-3`}>
-              {description}
-            </p>
-          ) : null}
-          {txid ? (
-            <a
-              href={
-                'https://explorer.solana.com/tx/' + txid + '?cluster=' + CLUSTER
-              }
-              className="mt-1 flex items-center text-sm"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="flex-1 break-all text-xs">
-                {type === 'error'
-                  ? txid
-                  : `${txid.slice(0, 14)}...${txid.slice(txid.length - 14)}`}
-              </div>
-              <ExternalLinkIcon className="mb-0.5 ml-1 h-4 w-4" />
-            </a>
-          ) : null}
-        </div>
-        <div className={`absolute right-2 top-2 flex-shrink-0`}>
-          <button
-            onClick={hideNotification}
-            className={`text-th-fgd-4 hover:text-th-primary focus:outline-none`}
-          >
-            <span className={`sr-only`}>{t('close')}</span>
-            <svg
-              className={`h-5 w-5`}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
+      <div
+        className={`pointer-events-auto mt-2 w-full max-w-sm overflow-hidden rounded-md border border-th-bkg-4 bg-th-bkg-3 shadow-lg ring-1 ring-black ring-opacity-5`}
+      >
+        <div className={`relative flex items-center px-2 py-2.5`}>
+          <div className={`flex-shrink-0`}>
+            {type === 'success' ? (
+              <CheckCircleIcon className={`mr-1 h-7 w-7 text-th-green`} />
+            ) : null}
+            {type === 'info' && (
+              <InformationCircleIcon
+                className={`mr-1 h-7 w-7 text-th-primary`}
               />
-            </svg>
-          </button>
+            )}
+            {type === 'error' && (
+              <XCircleIcon className={`mr-1 h-7 w-7 text-th-red`} />
+            )}
+            {type === 'confirm' && (
+              <Loading className="mr-1 h-7 w-7 text-th-fgd-3" />
+            )}
+          </div>
+          <div className={`ml-2 flex-1`}>
+            <div className={`text-normal font-bold text-th-fgd-1`}>
+              {parsedTitle || title}
+            </div>
+            {description ? (
+              <p className={`mb-0 mt-0.5 leading-tight text-th-fgd-3`}>
+                {description}
+              </p>
+            ) : null}
+            {txid ? (
+              <a
+                href={
+                  'https://explorer.solana.com/tx/' +
+                  txid +
+                  '?cluster=' +
+                  CLUSTER
+                }
+                className="mt-1 flex items-center text-sm"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="flex-1 break-all text-xs">
+                  {type === 'error'
+                    ? txid
+                    : `${txid.slice(0, 14)}...${txid.slice(txid.length - 14)}`}
+                </div>
+                <ExternalLinkIcon className="mb-0.5 ml-1 h-4 w-4" />
+              </a>
+            ) : null}
+          </div>
+          <div className={`absolute right-2 top-2 flex-shrink-0`}>
+            <button
+              onClick={hideNotification}
+              className={`text-th-fgd-4 hover:text-th-primary focus:outline-none`}
+            >
+              <span className={`sr-only`}>{t('close')}</span>
+              <svg
+                className={`h-5 w-5`}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Transition>
   )
 }
 
