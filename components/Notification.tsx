94fbca60ee7c3984cf2a472cd6f385fa41efeac4
tjs <tjshipe@gmail.com>
2022-04-12 10:53:50 -0400

    notification timeout should match client timeout

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -5,7 +5,10 @@ import {
   InformationCircleIcon,
   XCircleIcon,
 } from '@heroicons/react/outline'
-import useMangoStore, { CLUSTER } from '../stores/useMangoStore'
+import useMangoStore, {
+  CLIENT_TX_TIMEOUT,
+  CLUSTER,
+} from '../stores/useMangoStore'
 import { Notification, notify } from '../utils/notifications'
 import { useTranslation } from 'next-i18next'
 import Loading from './Loading'
@@ -104,7 +107,9 @@ const Notification = ({ notification }: { notification: Notification }) => {
           hideNotification()
         }
       },
-      parsedTitle || type === 'confirm' || type === 'error' ? 40000 : 8000
+      parsedTitle || type === 'confirm' || type === 'error'
+        ? CLIENT_TX_TIMEOUT
+        : 8000
     )
 
     return () => {
