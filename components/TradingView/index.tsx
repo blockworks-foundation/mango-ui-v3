a6082b92a03b6acdee1d0bf2145e8ce3b005d69e
saml33 <30796577+saml33@users.noreply.github.com>
2022-04-15 22:07:38 +1000

    Revert "add tradingview chart attribution link (#257)"
    This reverts commit be0995af25bb66e7354783abc223819bb16d6d83.

--- components/TradingView/index.tsx
+++ components/TradingView/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useTheme } from 'next-themes'
 import {
   widget,
@@ -623,25 +623,7 @@ const TVChartContainer = () => {
     }
   }, [chartReady, openOrders, showOrderLines])
 
-  const attributionUrl = useMemo(
-    () =>
-      `https://tradingview.com/symbols/${defaultProps.symbol?.slice(0, -5)}USD`,
-    [defaultProps]
-  )
-
-  return (
-    <>
-      <div id={defaultProps.containerId} className="tradingview-chart" />
-      <a
-        className="default-transition tiny-text flex justify-end px-3 py-1 text-th-fgd-4 hover:text-th-fgd-3"
-        href={attributionUrl}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        Chart by TradingView
-      </a>
-    </>
-  )
+  return <div id={defaultProps.containerId} className="tradingview-chart" />
 }
 
 export default TVChartContainer
