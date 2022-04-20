91a7f8a6336a1bb3be7606413c2d89a1eff2a1df
tjs <tjshipe@gmail.com>
2022-04-20 17:33:44 -0400

    enable share modal if avgEntryPrice is available

--- components/PerpPositionsTable.tsx
+++ components/PerpPositionsTable.tsx
@@ -248,7 +248,7 @@ const PositionsTable: React.FC = () => {
                               onClick={() =>
                                 handleShowShare(openPositions[index])
                               }
-                              disabled={!breakEvenPrice ? true : false}
+                              disabled={!avgEntryPrice ? true : false}
                             >
                               <TwitterIcon className="h-4 w-4" />
                             </LinkButton>
