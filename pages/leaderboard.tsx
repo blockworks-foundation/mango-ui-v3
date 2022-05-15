02199e08546acee744dc035da87278d76afd0319
saml33 <slam.uke@gmail.com>
2022-05-16 08:40:06 +1000

    fix translations

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -3,6 +3,17 @@ import PageBodyContainer from '../components/PageBodyContainer'
 import TopBar from '../components/TopBar'
 import LeaderboardTable from '../components/LeaderboardTable'
 import ButtonGroup from 'components/ButtonGroup'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      // Will be passed to the page component as props
+    },
+  }
+}
 
 const leaderboardRangePresets = [
   { label: '7d', value: '7' },
@@ -14,6 +25,7 @@ const leaderboardRangePresetValues = leaderboardRangePresets.map((x) => x.value)
 
 export default function Leaderboard() {
   const [leaderboardRange, setLeaderboardRange] = useState('30')
+  const { t } = useTranslation('common')
 
   return (
     <div className={`bg-th-bkg-1 text-th-fgd-1 transition-all`}>
@@ -21,7 +33,7 @@ export default function Leaderboard() {
       <PageBodyContainer>
         <div className="flex flex-col pt-8 pb-3 sm:flex-row sm:items-center sm:justify-between sm:pb-6 md:pt-10">
           <h1 className={`mb-4 text-2xl font-semibold text-th-fgd-1 sm:mb-0`}>
-            Leaderboard
+            {t('leaderboard')}
           </h1>
           <div className="w-full sm:ml-auto sm:w-56">
             <ButtonGroup
