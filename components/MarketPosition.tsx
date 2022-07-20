f226e210ce133f2a44a6afbe11bcbed04595a26c
saml33 <30796577+saml33@users.noreply.github.com>
2022-07-20 20:19:59 +1000

    add market close to positions table (#365)
    * add market close to positions table
    
    * fix variable reference
    
    * use secondary buttons

--- components/MarketPosition.tsx
+++ components/MarketPosition.tsx
@@ -390,6 +390,7 @@ export default function MarketPosition() {
           <Button
             onClick={() => setShowMarketCloseModal(true)}
             className="mt-2.5 w-full"
+            primary={false}
           >
             <span>{t('market-close')}</span>
           </Button>
@@ -399,8 +400,7 @@ export default function MarketPosition() {
         <MarketCloseModal
           isOpen={showMarketCloseModal}
           onClose={handleCloseWarning}
-          market={selectedMarket}
-          marketIndex={marketIndex}
+          position={{ marketConfig: marketConfig, perpMarket: selectedMarket }}
         />
       ) : null}
     </>
